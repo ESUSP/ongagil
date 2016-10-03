9102b7dddff7f90a1a9f7749e76ff707d78c9eed
Ricardo Takazu <ricardo.takazu@hotmail.com>
2016-10-03 18:58:12 +0000

    teste unitario modelo doador

--- spec/models/doador_spec.rb
+++ spec/models/doador_spec.rb
@@ -1,9 +1,30 @@
-=begin
 
 require 'rails_helper'
 
 RSpec.describe Doador, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "Validade do Doador" do
+      it "Doador válido?" do
+          doador = Doador.new
+          doador.nome = "Ricardo"
+          doador.endereco = "Rua Legião Urbana, 256"
+          doador.telefone = 23457765
+          
+          expect(doador.valid?).to be_truthy
+      end 
+      
+      it "Doador inválido?" do
+          doador = Doador.new
+          doador.nome = nil
+          doador.endereco = "Rua Marcos Nazi, 256"
+          doador.telefone = 23457765
+          
+          expect(doador.valid?).to be_falsey
+      end 
+   end 
 end
 
-=end
+
+
+
