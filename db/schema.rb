bac106a611495fee416de66024efec32a4b9e692
Ricardo Takazu <ricardo.takazu@hotmail.com>
2016-10-05 14:07:52 +0000

    corrigido input telefone doador reposicionado campos

--- db/schema.rb
+++ db/schema.rb
@@ -34,13 +34,6 @@ ActiveRecord::Schema.define(version: 20160930150213) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "beneficiarios", force: :cascade do |t|
-    t.string   "nome"
-    t.string   "telefone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "doadors", force: :cascade do |t|
     t.string   "nome"
     t.text     "endereco"
