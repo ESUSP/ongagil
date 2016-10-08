8f7d70dd5af12a9bde073c227391f097d4cb837a
Rafael Sandroni <rafaelsandronidias@gmail.com>
2016-10-08 10:56:24 -0400

    Validation Form Projeto/New

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -2,6 +2,5 @@ class Projeto < ApplicationRecord
   validates_presence_of :Projeto #Nome do Projeto
   validates_presence_of :RespNome #Nome do Responsavel
   validates_presence_of :Ong #Nome da ONG
-  validates_presence_of :Tipo #Tipo do Projeto
-  validates_presence_of :DataInicio #Data Inicio do Projeto
+  validates_presence_of :Tipo #Tipo do Projeto  
 end
