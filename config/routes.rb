a0f2ba6bac0e2d6a69e37317a59e5cf94e8e8994
Ligia santos <ligia.santos01@fatec.sp.gov.br>
2016-10-11 20:49:22 +0000

    feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,13 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-#<<<<<<< HEAD
   resources :projetos
   root 'welcome#index'
-#=======
+
   resources :doadors
   resources :beneficiaries
   get '/beneficiaries/new', to: 'beneficiaries#new', as: 'beneficiaries_new'
   get '/beneficiaries/edit', to: 'beneficiaries#edit', as: 'beneficiaries_edit'
-#>>>>>>> 9102b7dddff7f90a1a9f7749e76ff707d78c9eed
-  
   
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
@@ -19,7 +16,5 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :usuarios
-  
-  
   
 end
