2a0378fd4550cfd591af4cfb4e0c84a388841893
Rafael Sandroni <rafaelsandronidias@gmail.com>
2016-10-24 14:36:47 -0400

    Inclusao de seeds para o modulo projeto

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,31 @@ end
 defaultadmin = Usuario.create(email: 'admin@admin.com', nome:'Usuario Administrador', password:'12345', password_confirmation:'12345')
 genu(30)
 
+
+#seeds for projeto
+
+Projeto.destroy_all
+
+
+Projeto.create(Projeto: "Minha Cada Minha Vida #1",
+	                RespNome: "#{['José','Maria','João'].sample}",
+	                Ong: "Ong Test",
+									OngNumero: 1,
+	                Tipo:  "Moradia" ,
+	                DescricaoProjeto: "Projeto teste",
+									Status: "Ativo",
+									Area: "Teste",
+									DataInicio: Date.today.at_beginning_of_month,
+									DataFim: Date.today.at_beginning_of_month.next_month )
+
+Projeto.create(Projeto: "CriançaFeliz",
+	                RespNome: "#{['Testador', 'Testudo','Testão'].sample}",
+	                Ong: "Ong Test",
+									OngNumero: 2,
+	                Tipo:  "Social" ,
+	                DescricaoProjeto: "Campanha de Arrecadação de Brinquedos",
+									Status: "Ativo",
+									Area: "Teste",
+									DataInicio: Date.today.at_beginning_of_month,
+									DataFim: Date.today.at_beginning_of_month.next_month)
+
