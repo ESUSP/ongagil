55c4fe9f8b57fe5b697a275dfcddc4bc649c6696
Ricardo Takazu <ricardo.takazu@hotmail.com>
2016-11-28 10:53:48 +0000

    teste de associação da Doação

--- spec/models/doacao_spec.rb
+++ spec/models/doacao_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Doacao, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  
+    context "Validade da Doação" do
+        it "Doação deve pertencer a Doador" do
+            d = Doacao.reflect_on_association(:doador)
+            d.macro.should == :belongs_to
+        end
+    end
 end
